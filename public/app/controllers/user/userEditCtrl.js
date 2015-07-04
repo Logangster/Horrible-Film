214091b1b10d51ad78d39349231ea42eeff9ac02
Logan Gore <rockstar171@gmail.com>
2015-07-03 21:08:38 -0600

    Removed test code

--- public/app/controllers/user/userEditCtrl.js
+++ public/app/controllers/user/userEditCtrl.js
@@ -2,9 +2,7 @@ angular.module('horribleFilm')
 .controller('UserEditCtrl', function($routeParams, User) {
 	
 	var vm = this;
-	
-	vm.profile = {aboutMe: 'testestset'};
-	
+
 	User.get($routeParams.user_name).then(function(response) {
 		vm.profile = response.data.Profile;
 	});
